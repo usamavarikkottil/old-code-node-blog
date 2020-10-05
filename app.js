391bd3801f5b7fac90fbeda1785b180652ee1dd3
usama v <varikkottilusama@gmail.com>
2020-10-05 17:56:07 +0530

    replace body parser with express

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 //jshint esversion:6
-
 const express = require("express");
-const bodyParser = require("body-parser");
 const _ = require("lodash");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
@@ -15,7 +13,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
