ede731780d521427caa655fac7c4961653af6111
usama v <varikkottilusama@gmail.com>
2020-10-06 10:07:38 +0530

    add env port

--- app.js
+++ app.js
@@ -77,4 +77,4 @@ app.post("/compose", (req, res) => {
 
 
 //Listener
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(process.env.PORT, () => console.log("Server started on port 3000"));
